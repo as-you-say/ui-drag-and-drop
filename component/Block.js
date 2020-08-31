c5264b58e8e60e069ffaf41963ec762835d03565
홍지성 <dgf5820@daum.net>
2020-08-31 17:34:11 +0900

    # 수정 - 블럭 양 끝점 마우스오버 옵션과 함께 추가

--- component/Block.js
+++ component/Block.js
@@ -11,6 +11,7 @@ var Block = function(target, innerHTML){
     block.style.position = 'absolute';
     block.innerHTML = innerHTML;
 
+    // Event - Block Shadow
     blockMenu.addEventListener('dragstart', function(e){
         var img = document.createElement("img");
         e.dataTransfer.setDragImage(img, 0, 0);
@@ -21,12 +22,20 @@ var Block = function(target, innerHTML){
         block.style.top = e.pageY - block.clientHeight/2 + 'px';
     })
     blockMenu.addEventListener('dragend', function(e){
+        // Dom - Block Shadow
         block.style.left = e.pageX - block.clientWidth/2 + 'px';
         block.style.top = e.pageY - block.clientHeight/2 + 'px';
 
+        // Dom - Block + prev + next
         var newBlock = block.cloneNode(true);
+        var prev = document.createElement('div');
+        var next = document.createElement('div');
+        newBlock.appendChild(prev);
+        newBlock.appendChild(next);
         document.body.appendChild(newBlock);
         block.style.opacity = 0;
+        
+        // Event - Block
         newBlock.addEventListener('dragstart', function(e2){
             var img = document.createElement("img");
             e2.dataTransfer.setDragImage(img, 0, 0);
@@ -38,6 +47,24 @@ var Block = function(target, innerHTML){
         newBlock.addEventListener('dragend', function(e2){
             newBlock.style.left = e2.pageX - newBlock.clientWidth/2 + 'px';
             newBlock.style.top = e2.pageY - newBlock.clientHeight/2 + 'px';
+        })
+
+        // Event - prev
+        prev.className = 'point prev';
+        prev.addEventListener('click', function(e){
+            prev.style.backgroundColor = '#ddee00';
+            prev.style.width = '20px';
+            prev.style.height = '20px';
+            prev.style.left = '-10px';
+        })
+
+        // Event - next
+        next.className = 'point next';
+        next.addEventListener('click', function(e){
+            next.style.backgroundColor = '#ddee00';
+            next.style.width = '20px';
+            next.style.height = '20px';
+            next.style.right = '-10px';
         })
     })
 }
